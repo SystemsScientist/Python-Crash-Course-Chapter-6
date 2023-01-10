b69a4879d10f8209cac53bc6f1bfc223239a8bd0
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-10 17:42:30 -0600

    Adding alien3.py file

---
+++ Examples/alien3.py
@@ -0,0 +1,11 @@
+# alien3.py, Chapter 6, Python Crash Course
+# program initializes a simple dictionary
+# and then prints the points
+
+alien_0 = {'color': 'green', 'points': 5}
+
+new_points = alien_0['points']
+print("You just earned " + str(new_points) + " points!")
+
+
+

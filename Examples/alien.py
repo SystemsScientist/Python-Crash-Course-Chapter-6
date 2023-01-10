a2338e89b263bf2a64d7cc10acc2cc032c507db0
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-09 19:00:40 -0600

    Adding alien.py file

---
+++ Examples/alien.py
@@ -0,0 +1,13 @@
+# alien.py, Chapter 6, Python Crash Course
+# program initializes a simple dictionary
+# and then prints the alien's color and 
+# points.
+
+alien_0 = {'color': 'green', 'points': 5}
+
+print(alien_0['color'])
+print(alien_0['points'])
+
+
+
+

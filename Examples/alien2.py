8dc5c65e32aefac462b0773e5c425f8ce54330de
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-10 17:38:56 -0600

    Adding alien2.py file

---
+++ Examples/alien2.py
@@ -0,0 +1,10 @@
+# alien2.py, Chapter 6, Python Crash course
+# program initializes a simple dictionary
+# and then prints the alien's color
+
+alien_0 = {'color': 'green'}
+
+print(alien_0['color'])
+
+
+

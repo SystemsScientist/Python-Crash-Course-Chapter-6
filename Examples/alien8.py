b50590613f504d36de7efe4a002712edabe6fe25
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-11 17:43:36 -0600

    Adding alien8.py file

---
+++ Examples/alien8.py
@@ -0,0 +1,11 @@
+# alien8.py, Chapter 6, Python Crash Course
+# program removes data from a dictionary
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+del alien_0['points']
+print(alien_0)
+
+
+

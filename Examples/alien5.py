b89edf7762a8e5f4b1690aa0de142ed26f7ece46
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-11 17:21:47 -0600

    Adding alien5.py file

---
+++ Examples/alien5.py
@@ -0,0 +1,13 @@
+# alien5.py, Chapter 6, Python Crash Course
+# program adds to an empty dictionary and
+# then prints the dictionary
+
+alien_0 = {}
+
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+
+print(alien_0)
+
+
+

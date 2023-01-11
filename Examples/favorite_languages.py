4ad24642bd84a0bf8bc82ebcfa3db963c1066646
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-11 17:51:02 -0600

    Adding favorite_languages.py file

---
+++ Examples/favorite_languages.py
@@ -0,0 +1,17 @@
+# favorite_languages.py, Chapter 6, Python Crash Course
+# program initializes a dictionary of users and favorite
+# programming languages and then prints one of the user's
+# favorite programming languages
+
+favorite_languages = {
+        'jen': 'python',
+        'sarah': 'c',
+        'edward': 'ruby',
+        'phil': 'python',
+        }
+
+print("Sarah's favorite language is " + 
+      favorite_languages['sarah'].title() + ".")
+
+
+

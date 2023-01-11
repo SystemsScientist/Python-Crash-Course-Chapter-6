44eaf55996bc358415ab53ac7a557bd77191763b
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-11 17:26:02 -0600

    Adding alien6.py file

---
+++ Examples/alien6.py
@@ -0,0 +1,12 @@
+# alien6.py, Chapter 6, Python Crash Course
+# program modifies a dicitonary and then
+# prints the dictionary
+
+alien_0 = {'color': 'green'}
+print("The alien is " + alien_0['color'] + ".")
+
+alien_0['color'] = 'yellow'
+print("The alien is now " + alien_0['color'] + ".")
+
+
+

fedcdc5c2d06f3c2889b47ee743a2f67507ed109
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-11 17:16:31 -0600

    Adding alien4.py file

---
+++ Examples/alien4.py
@@ -0,0 +1,14 @@
+# alien4.py, Chapter 6, Python Crash Course
+# program initializes a simple dictionary,
+# adds key-value pairs to the dictionary,
+# and then prints those values
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
+
+
